72b526fb05e8d4752519d0e2273806a9effe2f0d
zytabcd123 <zyt@ovfundemacbook-pro.local>
2015-12-21 14:12:15 +0800

    Initial Commit

---
+++ OO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OO
+//
+//  Created by ovfun on 15/12/21.
+//  Copyright © 2015年 @天意. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
